c38c684a66a2ffac3e4640f4aa3261ed44beadf8
temp <temp@STUD.UNV.SU>
2019-10-25 10:23:16 +0300

    Xterm

---
+++ src/Task.java
@@ -0,0 +1,8 @@
+import com.sun.org.apache.xerces.internal.xs.XSTerm;
+
+public class Task {
+    public static void main(String[] args) throws Exception {
+        int a = Xterm.inputInt("dsg->");
+        Xterm.println("До обмена " + a);
+    }
+}
